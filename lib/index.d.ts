0e0e691bcf65d1838dd26a8b04bb98c892802ac1
medzhidov <ilyamedzhidov@yandex.ru>
2020-12-08 05:55:46 +0300

    Build libs

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,46 +1,56 @@
 import { VueConstructor } from 'vue';
 
 // TODO: tiptap extensions
-declare class Doc {}
-declare class Paragraph {}
-declare class Text {}
-declare class Heading {}
-declare class Bold {}
-declare class Underline {}
-declare class Italic {}
-declare class Strike {}
-declare class Link {}
-declare class Blockquote {}
-declare class CodeBlock {}
-declare class Code {}
-declare class Image {}
-declare class Iframe {}
-declare class ListItem { }
-declare class BulletList { }
-declare class OrderedList { }
-declare class TodoItem { }
-declare class TodoList { }
-declare class Table { }
-declare class TableHeader { }
-declare class TableCell { }
-declare class TableRow {}
-declare class HardBreak {}
-declare class TrailingNode {}
-declare class TextAlign {}
-declare class Indent {}
-declare class LineHeight {}
-declare class HorizontalRule {}
-declare class History {}
-declare class TextColor {}
-declare class TextHighlight {}
-declare class FontType {}
-declare class FontSize {}
-declare class FormatClear {}
-declare class Fullscreen {}
-declare class Print {}
-declare class Preview {}
-declare class SelectAll {}
-declare class CodeView {}
+
+interface defaultOptions {
+  floating: boolean,
+  bubble: boolean
+}
+
+declare class ElementTiptapExtension {
+  constructor(options: defaultOptions)
+}
+
+declare class Doc extends ElementTiptapExtension {}
+declare class Paragraph extends ElementTiptapExtension {}
+declare class Text extends ElementTiptapExtension {}
+declare class Heading extends ElementTiptapExtension {}
+declare class Bold extends ElementTiptapExtension {}
+declare class Underline extends ElementTiptapExtension {}
+declare class Italic extends ElementTiptapExtension {}
+declare class Strike extends ElementTiptapExtension {}
+declare class Link extends ElementTiptapExtension {}
+declare class Blockquote extends ElementTiptapExtension {}
+declare class CodeBlock extends ElementTiptapExtension {}
+declare class Code extends ElementTiptapExtension {}
+declare class Image extends ElementTiptapExtension {}
+declare class Iframe extends ElementTiptapExtension {}
+declare class ListItem extends ElementTiptapExtension {}
+declare class BulletList extends ElementTiptapExtension {}
+declare class OrderedList extends ElementTiptapExtension {}
+declare class TodoItem extends ElementTiptapExtension {}
+declare class TodoList extends ElementTiptapExtension {}
+declare class Table extends ElementTiptapExtension {}
+declare class TableHeader extends ElementTiptapExtension {}
+declare class TableCell extends ElementTiptapExtension {}
+declare class TableRow extends ElementTiptapExtension {}
+declare class HardBreak extends ElementTiptapExtension {}
+declare class TrailingNode extends ElementTiptapExtension {}
+declare class TextAlign extends ElementTiptapExtension {}
+declare class Indent extends ElementTiptapExtension {}
+declare class LineHeight extends ElementTiptapExtension {}
+declare class HorizontalRule extends ElementTiptapExtension {}
+declare class History extends ElementTiptapExtension {}
+declare class TextColor extends ElementTiptapExtension {}
+declare class TextHighlight extends ElementTiptapExtension {}
+declare class FontType extends ElementTiptapExtension {}
+declare class FontSize extends ElementTiptapExtension {}
+declare class FormatClear extends ElementTiptapExtension {}
+declare class Fullscreen extends ElementTiptapExtension {}
+declare class Print extends ElementTiptapExtension {}
+declare class Preview extends ElementTiptapExtension {}
+declare class SelectAll extends ElementTiptapExtension {}
+declare class CodeView extends ElementTiptapExtension {}
 
 interface OptionsInterface {
   lang?: string;
